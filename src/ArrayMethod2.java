db943b0ef1a6ad8bbd53de2a6f9b8ad5d63d56f4
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2018-02-06 09:23:29 -0500

    f

--- src/ArrayMethod2.java
+++ src/ArrayMethod2.java
@@ -1,25 +1,33 @@
+import java.util.Arrays;
 
 public class ArrayMethod2 
 {
+	public static void main(String[] args)
+	{
+		String [] test1 = { "abba","bar","foo", "tortilla", "zebra"};
+		String [] test2 = {"amy", "bob", "claire", "sam", "tom"};
+	}
+	
 	public static String[] merge(String[] list1, String[] list2)
 	{
-		String[] newArray = new String[0];
-		for(int i = 0; i < list1.length; i++)
-		{
-			for(int j = 0; j < list2.length; j++)
-			{
-				if(list1[i].compareTo(list2[j]) > 0)
-				{
-					newArray[i] = list1[i];
-				}
-				else 
-				{
-					newArray[i] = list2[j];
-				}
-			}
-		}
-		return newArray;
-		
+		String[] newArray = new String[list1.length + list2.length];
+		String[] list3 = new String[list1.length + list2.length];
+	      int i = 0, j = 0, k = 0;
+	      while (i < list1.length && j < list2.length) 
+	      {
+	           if (list1[i].compareTo(list2[j]) <01) 
+	           {
+	               list3[k] = list1[i];
+	               i++;
+	           } 
+	           else 
+	           {
+	               list3[k] = list2[j];
+	               j++;
+	           }
+	           k++;
+	      }
+	      return list3;
 	}
 	
 	public static int partition(int[] list)
